34263a9d06f31520e257f9e4ae87a62299a29ef9
Alejandro <ale101rosales@gmail.com>
2025-03-08 21:00:01 -0600

    Actividad 2

---
+++ FRONTEND/prueba-next/app/StoreProvider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import App from "next/app";
+import React, { useRef } from "react";
+import { Provider } from "react-redux";
+import { AppStore, makeStore} from "../Redux/store";
+
+export default function StoreProvider({children}:{children: React.ReactNode}){
+    const store = useRef<AppStore | null>(null);
+    if(!store.current){
+        store.current = makeStore();
+    }
+
+    return <Provider store={store.current}>{children}</Provider>;
+}
+
